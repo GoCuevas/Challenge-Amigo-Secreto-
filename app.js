af8bf919942104bb45ec00608243c26d3e36a9f6
Gonzalo Cuevas <gocuevasll@gmail.com>
2025-03-12 23:47:27 -0300

    Actualiza código JS en app.js
    Se agrega función "Splice" para eliminar amigo ya sorteado en Array y evitar repetición de valor entregado.

--- app.js
+++ app.js
@@ -24,13 +24,29 @@ function renderAmigos(){
         listaAmigos.appendChild(item);
     }
 }
-
+/*
 function sortearAmigo(){
     if (amigo.length === 0){
         alert("No ingresaste amigos para el sorteo");
-        return;    
+        return;
+  
     }
     let amigoSecreto = amigo[Math.floor(Math.random() * amigo.length)];
     let resultado = document.getElementById("resultado");
     resultado.innerHTML = `EL amigo sorteado es: ${amigoSecreto}`;
 }
+
+*/
+function sortearAmigo() {
+    if (amigo.length === 0) {
+        alert("No quedan amigos por sortear");
+        return;
+    }
+
+    let indice = Math.floor(Math.random() * amigo.length);
+    let amigoSecreto = amigo.splice(indice, 1)[0]; // Extrae y elimina el amigo sorteado
+
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = `El amigo sorteado es: ${amigoSecreto}`;
+    
+    renderAmigos(); // Actualiza la lista visible para reflejar los cambios
